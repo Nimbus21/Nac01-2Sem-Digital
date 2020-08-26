04ffbfad49862b7f79f9bffa5a36473b4893f854
Nimbus21 <thandy_97@hotmail.com>
2020-08-26 18:41:39 -0300

    classes renomeadas

--- src/main/java/br/com/fiap/capsuledev/Nac012SemDigital.java
+++ src/main/java/br/com/fiap/capsuledev/Nac012SemDigital.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Checkpoint012SemDigitalApplication {
+public class Nac012SemDigital {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Checkpoint012SemDigitalApplication.class, args);
+		SpringApplication.run(Nac012SemDigital.class, args);
 	}
 
 }
--- src/test/java/br/com/fiap/capsuledev/Nac012SemDigitalTests.java
+++ src/test/java/br/com/fiap/capsuledev/Nac012SemDigitalTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Checkpoint012SemDigitalApplicationTests {
+class Nac012SemDigitalTests {
 
 	@Test
 	void contextLoads() {
