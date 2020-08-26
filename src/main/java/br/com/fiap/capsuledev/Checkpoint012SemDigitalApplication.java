0d6a5fbc5f29af4f853408db1fcc9a485bd59a2a
Nimbus21 <thandy_97@hotmail.com>
2020-08-26 17:20:24 -0300

    First commit

---
+++ src/main/java/br/com/fiap/capsuledev/Checkpoint012SemDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.capsuledev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkpoint012SemDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Checkpoint012SemDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/capsuledev/Checkpoint012SemDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.capsuledev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Checkpoint012SemDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
