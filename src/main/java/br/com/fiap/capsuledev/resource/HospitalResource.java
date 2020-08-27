13b1ea1935ec54700ba4220cff8f3d2226a61c8b
Nimbus21 <thandy_97@hotmail.com>
2020-08-27 17:37:48 -0300

    Funcionalidade adicionar

--- src/main/java/br/com/fiap/capsuledev/resource/HospitalResource.java
+++ src/main/java/br/com/fiap/capsuledev/resource/HospitalResource.java
@@ -1,16 +1,22 @@
 package br.com.fiap.capsuledev.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.fiap.capsuledev.domain.Hospital;
 import br.com.fiap.capsuledev.domain.dto.HospitalDTO;
@@ -25,16 +31,34 @@ public class HospitalResource {
 	
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
-	public List<HospitalDTO> lista(String nome) {
-		List<Hospital> hospitais = nome == null ? hospitalRepository.findAll() : hospitalRepository.findByNomeContaining(nome);
+	public List<HospitalDTO> listar(String nome) {
+		//List<Hospital> hospitais = nome == null ? hospitalRepository.findAll() : hospitalRepository.findByNomeContaining(nome);
+		List<Hospital> hospitais = null;
+		
+		if (nome == null) {
+			hospitais = hospitalRepository.findAll();
+		} else {
+			hospitais = hospitalRepository.findByNomeContaining(nome);
+		}
 		
 		return HospitalDTO.converter(hospitais);
 	}
 	
 	@GetMapping("{id}")
-	public ResponseEntity<HospitalDTO> detalhe(@PathVariable("id") Long codigo){
+	public ResponseEntity<HospitalDTO> listarId(@PathVariable("id") Long codigo){
 		Optional<Hospital> hospital = hospitalRepository.findById(codigo);
 		return hospital.map(h -> ResponseEntity.ok(new HospitalDTO(h))).orElse(ResponseEntity.notFound().build());
+	}
+	
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<HospitalDTO> adicionar(@RequestBody Hospital hospital, UriComponentsBuilder uriBuilder) {
+		hospitalRepository.save(hospital);
+		
+		URI uri = uriBuilder.path("/hospital/{id}").buildAndExpand(hospital.getCodigo()).toUri();
+		
+		return ResponseEntity.created(uri).body(new HospitalDTO(hospital));
 	}
 	
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO HOSPITAL (codigo, nome, nr_cnpj, inscricao_estadual, endereco, telefone) VALUES
   (SQ_HOSPITAL.nextval, 'Sírio Libanês', '1000', '2000', 'Rua seila', '11 978545545');
+  
+INSERT INTO HOSPITAL (codigo, nome, nr_cnpj, inscricao_estadual, endereco, telefone) VALUES
+  (SQ_HOSPITAL.nextval, 'Albert Einstein', '3000', '4000', 'Rua x', '11 999999999');
