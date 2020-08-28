aae0a8269ec6b1489298e5180ada43ccc4e79a9f
Nimbus21 <thandy_97@hotmail.com>
2020-08-28 20:13:35 -0300

    correcao sql de teste

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO HOSPITAL (codigo, nome, nr_cnpj, inscricao_estadual, endereco, telefone) VALUES
+INSERT INTO TB_HOSPITAL_CLINICA (cd_hospital_clinica, nm_hospital_clinica, nr_cnpj, nr_inscricao_estadual, ds_endereco, nr_telefone) VALUES
   (SQ_HOSPITAL.nextval, 'Sírio Libanês', '1000', '2000', 'Rua seila', '11 978545545');
   
-INSERT INTO HOSPITAL (codigo, nome, nr_cnpj, inscricao_estadual, endereco, telefone) VALUES
+INSERT INTO TB_HOSPITAL_CLINICA (cd_hospital_clinica, nm_hospital_clinica, nr_cnpj, nr_inscricao_estadual, ds_endereco, nr_telefone) VALUES
   (SQ_HOSPITAL.nextval, 'Albert Einstein', '3000', '4000', 'Rua x', '11 999999999');
