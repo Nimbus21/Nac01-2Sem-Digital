9a8dd352051137090bdeb8d27a21bcae1516bacc
Nimbus21 <thandy_97@hotmail.com>
2020-09-28 19:01:42 -0300

    horarios adicionados nos inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,16 +15,16 @@ INSERT INTO TB_PACIENTE (cd_paciente, nm_contato, ds_endereco, ds_genero, dt_nas
  (SQ_PACIENTE.nextval, 'mamãe', 'grande rua', 'masculino', '1999-03-04', 'Gui', 'guiMae', 'pé', 'xx34', '9947474', '2020-03-04');
 
  INSERT INTO TB_MONITORAMENTO (cd_monitoramento, st_monitoramento, ds_monitoramento, dt_fim, nr_frequencia_segundos, dt_inicio, cd_hospital, cd_medico, cd_paciente) VALUES
- (SQ_MONITORAMENTO.nextval, false, 'primeiro monitoramento Thandy', '2021-08-04', 1000, '1021-08-09', 1, 1, 1);
+ (SQ_MONITORAMENTO.nextval, false, 'primeiro monitoramento Thandy', '2021-08-04 21:58:00', 1000, '1021-08-09', 1, 1, 1);
  
   INSERT INTO TB_MONITORAMENTO (cd_monitoramento, st_monitoramento, ds_monitoramento, dt_fim, nr_frequencia_segundos, dt_inicio, cd_hospital, cd_medico, cd_paciente) VALUES
- (SQ_MONITORAMENTO.nextval, false, 'segundo monitoramento Thandy', '2021-10-04', 2000, '1022-08-09', 1, 1, 1);
+ (SQ_MONITORAMENTO.nextval, false, 'segundo monitoramento Thandy', '2021-10-04 22:11:00', 2000, '1022-08-09', 1, 1, 1);
  
   INSERT INTO TB_MONITORAMENTO (cd_monitoramento, st_monitoramento, ds_monitoramento, dt_fim, nr_frequencia_segundos, dt_inicio, cd_hospital, cd_medico, cd_paciente) VALUES
- (SQ_MONITORAMENTO.nextval, false, 'primeiro monitoramento Gui', '2021-08-04', 3000, '1021-08-09', 1, 1, 2);
+ (SQ_MONITORAMENTO.nextval, false, 'primeiro monitoramento Gui', '2021-08-04 11:53:00', 3000, '1021-08-09', 1, 1, 2);
  
   INSERT INTO TB_MONITORAMENTO (cd_monitoramento, st_monitoramento, ds_monitoramento, dt_fim, nr_frequencia_segundos, dt_inicio, cd_hospital, cd_medico, cd_paciente) VALUES
- (SQ_MONITORAMENTO.nextval, false, 'segundo monitoramento Gui', '2021-08-04', 4000, '1021-08-09', 1, 1, 2);
+ (SQ_MONITORAMENTO.nextval, false, 'segundo monitoramento Gui', '2021-08-04 03:24:19', 4000, '1021-08-09', 1, 1, 2);
 
  INSERT INTO TB_CAPSULE_WATCH (cd_capsule_watch, nr_batimento, dt_data_hora, nr_imc, nr_pressao, nr_temperatura, cd_monitoramento) VALUES
  (SQ_CAPSULE_WATCH.nextval, 4, '2021-08-04', 8.2, 11.1, 36.2, 1);
